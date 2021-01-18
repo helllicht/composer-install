1a40ee91911477256d9c7594a67b0fa57714321a
Domenic Gerhold <gerhold@helllicht.com>
2021-01-18 10:02:42 +0100

    Refactor;

--- install.sh
+++ install.sh
@@ -1,27 +1,43 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+
+for i in "$@"
+do
+case $i in
+    -p=*|--php=*)
+    PHP_VERSION="${i#*=}"
+    shift # past argument=value
+    ;;
+    -c=*|--composer=*)
+    COMPOSER_VERSION="${i#*=}"
+    shift # past argument=value
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
 
 # https://github.com/composer/getcomposer.org/commits/master
 COMMIT_SHA=e3e43bde99447de1c13da5d1027545be81736b27
 
 URL="https://raw.githubusercontent.com/composer/getcomposer.org/$COMMIT_SHA/web/installer"
 
-if [ -z "$ACTION_COMPOSER_VERSION" ]
+if [ -z "$PHP_VERSION" ]
 then
-	echo "Error: ACTION_COMPOSER_VERSION variable not set"
+	echo "Error: -p=? variable not set"
 	exit 1
 fi
 
-if [ -z "$ACTION_PHP_VERSION" ]
+if [ -z "$COMPOSER_VERSION" ]
 then
-	echo "Error: ACTION_PHP_VERSION variable not set"
+	echo "Error: -c=? variable not set"
 	exit 1
 fi
 
-echo "Chosen PHP Version $ACTION_PHP_VERSION"
-echo "Chosen Composer Version $ACTION_COMPOSER_VERSION"
+echo "Chosen PHP Version $PHP_VERSION"
+echo "Chosen Composer Version $COMPOSER_VERSION"
 
-wget $URL -O - -q | /usr/bin/php$ACTION_PHP_VERSION -- --quiet --$ACTION_COMPOSER_VERSION
+wget $URL -O - -q | /usr/bin/php$PHP_VERSION -- --quiet --$COMPOSER_VERSION
 
 mv composer.phar /usr/local/bin/composer
 
