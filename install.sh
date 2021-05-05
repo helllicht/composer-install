5006a977a03c97c613971f3a2880b09956f98b82
Domenic Gerhold <gerhold@helllicht.com>
2021-05-05 10:13:22 +0200

    Add new option working-directory;

--- install.sh
+++ install.sh
@@ -15,6 +15,10 @@ case $i in
     WITH_DEV="${i#*=}"
     shift # past argument=value
     ;;
+    -w=*|--working-dir=*)
+    WORKING_DIR="${i#*=}"
+    shift # past argument=value
+    ;;
     *)
           # unknown option
     ;;
@@ -38,6 +42,13 @@ then
 	exit 1
 fi
 
+INTERNAL_WORKING_DIR=""
+if [ ${#WORKING_DIR} -gt 0 ]
+then
+	echo "Set custom working dir: $WORKING_DIR"
+	INTERNAL_WORKING_DIR="--working-dir=$WORKING_DIR"
+fi
+
 WITH_DEV_COMMAND="--no-dev"
 if [ "$WITH_DEV" = "true" ]; then
   echo "Install composer requirements with dev dependencies!"
@@ -49,9 +60,9 @@ fi
 echo "Chosen PHP Version $PHP_VERSION"
 echo "Chosen Composer Version $COMPOSER_VERSION"
 
-wget $URL -O - -q | /usr/bin/php$PHP_VERSION -- --quiet --$COMPOSER_VERSION
+wget $URL -O - -q | /usr/bin/php"$PHP_VERSION" -- --quiet --"$COMPOSER_VERSION"
 
-/usr/bin/php$PHP_VERSION composer.phar install $WITH_DEV_COMMAND
+/usr/bin/php"$PHP_VERSION" composer.phar install "$WITH_DEV_COMMAND" "$INTERNAL_WORKING_DIR"
 
 rm composer.phar
 
